7134aaded47997d14bb4667fe5f5aceb8c1d5288
Lotus352000 <91876771+Lotus352000@users.noreply.github.com>
2021-11-28 20:09:18 +0700

    Add files via upload

---
+++ resources/lang/en/footer.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'AboutUs'    => 'About Us',
+    'address'    => 'No.1 Dai Co Viet, Bach Khoa, Hai Ba Trung, Hanoi.',
+    'FollowUs'   => 'Follow Us',
+    'visa'       => 'Visa method',
+    'mastercard' => 'MasterCard method',
+    'jcb'        => 'JCB method',
+    'cod'        => 'Cash on Delivery - COD.',
+    'vnpay'      => 'Provided by VNPAY.',
+    'payment'    => 'Payment Method',
+    'support'    => 'Customer Support',
+];
