27df42964884f445b0720e4c535bb0e8a318a052
Lotus352000 <91876771+Lotus352000@users.noreply.github.com>
2021-11-28 20:08:29 +0700

    Add files via upload

---
+++ config/payment.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'vnpay' => [
+        'vnp_TmnCode'    => '5DQOI6MP',
+        'vnp_HashSecret' => 'ENYKRDIXAPWACFLTSOTCKGLQIJTJEUBH',
+        'vnp_Url'        => 'http://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
+        'vnp_Returnurl'  => 'http://127.0.0.1:8000/xac-thuc-thanh-toan',
+        'vnp_Ipn'        => 'http://127.0.0.1:8000/ipn',
+    ],
+];
